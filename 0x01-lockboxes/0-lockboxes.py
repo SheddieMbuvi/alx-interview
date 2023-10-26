693bdad85d56366647f6a3503bcb4cb67b3725f9
SheddieMbuvi <sheddiembuvi@gmail.com>
2023-10-26 09:49:10 -0700

    Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,20 +9,21 @@ def canUnlockAll(boxes):
         return False
 
     if not isinstance(boxes, list):
+        pass
 
-        if len(boxes) == 0:
-            return False
+    if len(boxes) == 0:
+        return False
 
-        check = [0]
-        list_ing = [i for i in range(len(boxes))]
-        for in_check in check:
-            for in_boxes in boxes[in_check]:
-                if in_boxes not in check and in_boxes in list_ing:
-                    if in_boxes >= len(boxes):
-                        return False
-                    check.append(in_boxes)
+    check = [0]
+    list_ing = [i for i in range(len(boxes))]
+    for in_check in check:
+        for in_boxes in boxes[in_check]:
+            if in_boxes not in check and in_boxes in list_ing:
+                if in_boxes >= len(boxes):
+                    return False
+                check.append(in_boxes)
 
-                    if len(check) == len(boxes):
-                return True
-        else:
-            return False
+                if len(check) == len(boxes):
+                    return True
+    else:
+        return False
